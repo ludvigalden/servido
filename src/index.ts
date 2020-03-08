54424b487de5a4a7745f7a4384e09655a1218b9a
ludvigalden <aldenludvig@gmail.com>
2020-03-08 14:46:45 +0100

    additional exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export { ServiceAsync, ServiceConstructingStatus } from "./async";
-export { Service, requireService, forgoService, clearDependent, ServiceContext } from "./core";
-export { useService, useClearedMemo, useMemoEffect, ServiceProvider, ServiceContextProvider } from "./react";
+export { Service, requireService, forgoService, clearDependent, ServiceContext, Class, ServiceIdentifier } from "./core";
+export {
+    useService,
+    ServiceProvider,
+    ServiceContextProvider,
+    useClearedMemo,
+    useMemoEffect,
+    useServiceContext,
+    uniqueServiceDependent,
+} from "./react";
